372a6e0762437776c1d5762b1ba6871009d9c723
Harerimana Egide <egideharerimana085@gmail.com>
2025-08-01 00:51:24 +0200

    Saving user login state

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tugende/dto/user_doc_dto.dart';
 
 // Firebase Auth Service Provider
@@ -24,19 +26,39 @@ class UserState {
 }
 
 class UserStateNotifier extends StateNotifier<UserState> {
+  static const String _userKey = 'user';
   UserStateNotifier(super.state);
 
-  void signInUser(UserDocDto user, {bool isLoading = false}) {
+  Future<void> signInUser(UserDocDto user, {bool isLoading = false}) async {
     print('UserStateNotifier: signInUser called with user: ${user.toJson()}, isLoading: $isLoading');
     state = UserState(user, isLoading: isLoading);
+    await SharedPreferences.getInstance().then((prefs) {
+      prefs.setString(_userKey, state.user?.uid ?? '');
+    });
   }
 
-  void signOutUser() {
+  Future<void> signOutUser() async {
     state = UserState(null, isLoading: false);
+    await SharedPreferences.getInstance().then((prefs) {
+      prefs.remove(_userKey);
+    });
   }
 
-  void setLoading(bool isLoading) {
-    state = UserState(state.user, isLoading: isLoading);
+  Future<UserDocDto?> loadSavedUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString(_userKey);
+    if (userId != null) {
+      // Fetch user data from Firestore or any other source
+      final userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+      if (userDoc.exists) {
+        final userData = UserDocDto.fromJson(userDoc.data()!);
+        signInUser(userData, isLoading: false);
+        return userData;
+      } else {
+        signOutUser();
+      }
+    }
+    return null;
   }
 }
 
