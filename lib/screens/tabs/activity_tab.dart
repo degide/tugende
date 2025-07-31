21333202da5f509e9d3edb7ec51314cd21037837
AngeNicole <angenicolemukundwa22@gmail.com>
2025-07-31 15:52:49 +0200

    ft: adding activity tab

---
+++ lib/screens/tabs/activity_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ActivityTab extends StatefulWidget {
+  const ActivityTab({super.key});
+
+  @override
+  State<ActivityTab> createState() => _ActivityTabState();
+}
+
+class _ActivityTabState extends State<ActivityTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
